394dd34291daa07cc91d8ad0cf1820eda67ebb2d
narottam goyal <narottamgoyal66@gmail.com>
2016-05-01 17:01:57 +0530

    Added files via upload

---
+++ Angular2 App With Electron/app/app.ts
@@ -0,0 +1,27 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {Component, Pipe, PipeTransform} from 'angular2/core';
+import {NgFor} from 'angular2/common';
+import {config} from './config.service';
+import {playlistcomponent} from './playlist.component';
+import {video} from './video';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html',
+    directives: [playlistcomponent]
+})
+ 
+export class App {
+  mainHeading = config.Main_Heading;
+    videoList: Array<video>;
+
+    constructor()
+    {
+        this.videoList = [
+            new video(1,"Title 1", "V1", "Desc 1"),
+            new video(2, "Title 2", "V2", "Desc 2")
+        ];
+    }
+}
+
+bootstrap(App);
